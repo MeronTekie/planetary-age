014695acfd2ff344dd9405a8617726e15fd28f33
MERON G. TEKIE <ramenimo@MERONs-MacBook-Pro.local>
2022-04-15 15:24:47 -0700

    Connect the UI Logic with the HTML and Business logic

--- src/index.js
+++ src/index.js
@@ -2,20 +2,41 @@ import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
-// import Age from "./ageCalculator.js";
+import Age from "./ageCalculator.js";
 
 $(document).ready(function () {
   $("#age-calculator").submit(function (event) {
     event.preventDefault();
-    // let age = parseInt($("#age").val());
-    // let country = $("#country").val();
-    // let profession = $("#profession").val();
+    let age = parseInt($("#age").val());
+    let country = $("#country").val();
+    let profession = $("#profession").val();
 
-    // const myAge = new Age(age, country, profession);
-    // myAge.lifeExpancey();
-    // myAge.moreThanAverageAge();
+    const myAge = new Age(age, country, profession);
+    myAge.lifeExpancey();
+    myAge.moreThanAverageAge();
 
-    // if (myAge.earthAge < 82) {
-    // }
+    if (myAge.earthAge < 75) {
+      $("#result").removeClass("output");
+      $("#earth").text("Your Age  in Earth: " + myAge.earthAge);
+      $("#mercury").text("Your Age in Mercury: " + myAge.mercuryAge);
+      $("#venus").text("Your Age in Venus : " + myAge.venusAge);
+      $("#mars").text("Your Age in Mars : " + myAge.marsAge);
+      $("#jupiter").text("Your Age in  Jupiter: " + myAge.jupiterAge);
+    } else {
+      $("#result").removeClass("output");
+      $("#earth").text(
+        "Your Age  in Earth has exceeded by: " + myAge.earthAgeLeft
+      );
+      $("#mercury").text(
+        "Your Age in Mercury has exceeded by: " + myAge.mercuryAgeLeft
+      );
+      $("#venus").text(
+        "Your Age in Venus has exceeded by: " + myAge.venusAgeLeft
+      );
+      $("#mars").text("Your Age in Mars has exceeded by: " + myAge.marsAgeLeft);
+      $("#jupiter").text(
+        "Your Age in  Jupiter has exceeded by: " + myAge.jupiterAgeLeft
+      );
+    }
   });
 });
