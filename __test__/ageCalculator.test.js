04900e20e60cb02ac569b76f33d74f18dcd888ec
MERON G. TEKIE <ramenimo@MERONs-MacBook-Pro.local>
2022-04-15 10:09:28 -0700

    The Age object test passed taking the age parameter

--- __test__/ageCalculator.test.js
+++ __test__/ageCalculator.test.js
@@ -1,1 +1,7 @@
-
+import Age from "./../src/ageCalculator.js";
+describe("Age", () => {
+  test("Should successfully take the arguement and return the value to be the Earth age", () => {
+    const ages = new Age(24);
+    expect(ages.earthAge).toEqual(24);
+  });
+});
--- src/ageCalculator.js
+++ src/ageCalculator.js
@@ -1,4 +1,9 @@
-export default function Age(age) {
- 
+export default class Age {
+  constructor(age) {
+    this.earthAge = age;
+    this.mercuryAge = 0.24 * age;
+    this.venusAge = 0.62 * age;
+    this.marsAge = 1.88 * age;
+    this.jupiterAge = 11.86 * age;
+  }
 }
-
